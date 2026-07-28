d5b690118da12328ce7f25dac1255d589f7cb30d
brianbis <brianbis@gmail.com>
2026-07-28 00:04:11 -0700

    NixOS configuration update

--- firefox/addons.nix
+++ firefox/addons.nix
@@ -1,4 +1,20 @@
 { pkgs, ... }:
+
+let
+  oled-borderless-pitch-black =
+    pkgs.nur.repos.rycee.firefox-addons.buildFirefoxXpiAddon {
+      pname = "oled-borderless-pitch-black";
+      version = "1.2";
+      addonId = "oled-borderless-pitch-black@mozilla.org";
+      url = "https://addons.mozilla.org/firefox/downloads/file/3492728/oled_borderless_pitch_black-1.2.xpi";
+      sha256 = "sha256-rkr8hBFkbjjl3L+UDDa+vLwO3LQquGC+B7ThEXdemIw=";
+
+      meta = {
+        homepage = "https://addons.mozilla.org/en-US/firefox/addon/oled-borderless-pitch-black/";
+        description = "OLED Borderless Pitch Black Firefox theme";
+      };
+    };
+in
 {
   packages = with pkgs.nur.repos.rycee.firefox-addons; [
     sponsorblock
@@ -11,5 +27,7 @@
     old-reddit-redirect
     betterttv
     tree-style-tab
+
+    oled-borderless-pitch-black
   ];
 }
--- firefox/settings.nix
+++ firefox/settings.nix
@@ -4,4 +4,9 @@
 
   # Allow fullscreen without widget restrictions
   "full-screen-api.ignore-widgets" = true;
+
+  "browser.uiCustomization.state" = builtins.readFile ./toolbar-state.json;
+
+  "extensions.autoDisableScopes" = 0;
+  "extensions.enabledScopes" = 15;
 }
