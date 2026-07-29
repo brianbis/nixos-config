b9069ee3e39fb949348697befd212b15033e1ba4
brianbis <brianbis@gmail.com>
2026-07-29 03:43:23 -0700

    fixed firefox theme

--- home/firefox/default.nix
+++ home/firefox/default.nix
@@ -4,6 +4,15 @@
   programs.firefox = {
     enable = true;
 
+    policies = {
+      ExtensionSettings = {
+        "{47400ad1-545d-449a-85f6-f9edfc7e590a}" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/file/3492728/oled_borderless_pitch_black-1.2.xpi";
+          installation_mode = "force_installed";
+        };
+      };
+    };
+
     profiles.main = {
       extensions.packages =
         import ./addons.nix { inherit pkgs nur; };
--- home/plasma.nix
+++ home/plasma.nix
@@ -4,6 +4,23 @@
   programs.plasma = {
     enable = true;
 
+    shortcuts = {
+      # 1. Bind standalone Meta, Meta+R, and Alt+Space directly to open KRunner
+      "services/org.kde.krunner.desktop" = {
+        "_launch" = [ "Meta" "Meta+R" "Alt+Space" ];
+      };
+
+      # 2. Unbind Spectacle's screen recording shortcut so it stops stealing Meta+R
+      "services/org.kde.spectacle.desktop" = {
+        "RecordRegion" = "none";
+      };
+
+      # 3. Prevent Kickoff from capturing Meta
+      "plasmashell" = {
+        "activate application launcher" = "none";
+      };
+    };
+
     configFile = {
       kwinrc = {
         Windows = {
